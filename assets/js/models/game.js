152763814b4ed243f04ca97fb8181e6401ed39de
Manuela Ghiulai <gmanuelaromina@gmail.com>
2021-05-11 21:14:49 +0200

    fix: game over position. 3rd try

--- assets/js/models/game.js
+++ assets/js/models/game.js
@@ -76,8 +76,8 @@ class Game {
           this.background.draw()
         this.ctx.drawImage(
             this.gameOverImg,
-            this.ctx.canvas.width / 2 - this.gameOverImg.width ,
-            this.ctx.canvas.width / 2 - this.gameOverImg.height,
+            (this.ctx.canvas.width - this.gameOverImg.width) / 2,
+            (this.ctx.canvas.width - this.gameOverImg.height) / 2,
             this.gameOverImg.width,
             this.gameOverImg.height
         );
